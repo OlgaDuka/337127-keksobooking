496295b100b7b1bebdd283743535d867230eda27
Olga Duka <olgaduk@mail.ru>
2017-11-24 23:06:10 +0600

    Создала DOM-элемент карточки

--- js/map.js
+++ js/map.js
@@ -11,19 +11,29 @@ var OFFER_TITLES = ['Большая уютная квартира',
 var OFFER_TYPES = ['flat', 'house', 'bungalo'];
 var OFFER_CHECKS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var MAX_ROOMS = 6;
+var MAX_GUESTS = 10;
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var MAX_PINS = 8;
+var coords = {
+  x: {
+    min: 300,
+    max: 900
+  },
+  y: {
+    min: 100,
+    max: 500
+  }
+};
 
 var offerTitles = OFFER_TITLES.slice();
-
 var getRandomValueForArr = function (arr) {
   return Math.floor(Math.random() * (arr.length));
 };
 
 var getRandomInt = function (minValue, maxValue) {
   return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
-};
-
-var generateAvatar = function (numAvatar) {
-  return 'img/avatars/user0' + numAvatar + '.png';
 };
 
 var generateTitle = function (arr) {
@@ -44,20 +54,20 @@ var generateFeatures = function () {
 
 var ads = [];
 
-for (var i = 0; i < 8; i++) {
-  var locationX = getRandomInt(300, 900);
-  var locationY = getRandomInt(100, 500);
+for (var i = 0; i < MAX_PINS; i++) {
+  var locationX = getRandomInt(coords.x.min, coords.x.max);
+  var locationY = getRandomInt(coords.y.min, coords.y.max);
   ads[i] = {
     author: {
-      avatar: generateAvatar(i + 1)
+      avatar: 'img/avatars/user0' + (i + 1) + '.png',
     },
     offer: {
       title: generateTitle(offerTitles),
       address: locationX + ', ' + locationY,
-      price: getRandomInt(1000, 1000000),
+      price: getRandomInt(MIN_PRICE, MAX_PRICE),
       type: OFFER_TYPES[getRandomInt(0, 3)],
-      rooms: getRandomInt(1, 6),
-      guests: getRandomInt(1, 10),
+      rooms: getRandomInt(1, MAX_ROOMS),
+      guests: getRandomInt(1, MAX_GUESTS),
       checkin: OFFER_CHECKS[getRandomInt(0, 3)],
       checkout: OFFER_CHECKS[getRandomInt(0, 3)],
       features: generateFeatures(),
@@ -80,9 +90,8 @@ var mapPinTemplate = document.querySelector('template').content.querySelector('.
 var renderMapPin = function (ad) {
   var mapPinElement = mapPinTemplate.cloneNode(true);
   mapPinElement.querySelector('img').src = ad.author.avatar;
-  mapPinElement.querySelector('button').style.left = ad.location.x + 'px;';
-  mapPinElement.querySelector('button').style.top = ad.location.y + 'px;';
-
+  mapPinElement.style.left = ad.location.x + 'px';
+  mapPinElement.style.top = ad.location.y + 'px';
   return mapPinElement;
 };
 
@@ -91,3 +100,32 @@ for (i = 0; i < ads.length; i++) {
   fragment.appendChild(renderMapPin(ads[i]));
 }
 listPins.appendChild(fragment);
+
+var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
+
+var renderMapCard = function (ad) {
+  var mapCardElement = mapCardTemplate.cloneNode(true);
+  mapCardElement.querySelector('h3').textContent = ad.offer.title;
+//  mapCardElement.querySelector('popup__price').textContent = toString(ad.offer.price) + '&#x20bd;/ночь';
+  mapCardElement.querySelector('small').textContent = ad.offer.address;
+  switch (ad.offer.type) {
+    case 'flat':
+      mapCardElement.querySelector('h4').textContent = 'Квартира';
+      break;
+    case 'bungalo':
+      mapCardElement.querySelector('h4').textContent = 'Бунгало';
+      break;
+    case 'house':
+      mapCardElement.querySelector('h4').textContent = 'Дом';
+  }
+  var mapCardP = mapCardElement.querySelectorAll('p');
+  mapCardP[2].textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+  mapCardP[3].textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  mapCardP[4].textContent = ad.offer.description;
+//  mapCardElement.querySelector('.popup__features') - тут будет формироваться список. Отдельной функцией лучше?
+  return mapCardElement;
+};
+
+fragment = document.createDocumentFragment();
+fragment.appendChild(renderMapCard(ads[0]));
+mapStart.appendChild(fragment);
