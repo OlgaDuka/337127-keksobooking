c53839e7f1455f86ea264b7fd6c1892762e2d734
Olga Duka <olgaduk@mail.ru>
2017-12-04 16:44:55 +0600

    Доработала функции с селектами, добавила красные рамки

--- js/map.js
+++ js/map.js
@@ -51,10 +51,10 @@ var offerTypePrice = {
 };
 // Объект соответствия количества комнат количеству возможных гостей
 var capacityOfRooms = {
-  0: [2],
-  1: [1, 2],
-  2: [0, 1, 2],
-  3: [3]
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0]
 };
 
 // Массив объектов недвижимости
@@ -269,15 +269,37 @@ var roomNamberHousing = formNotice.querySelector('#room_number');
 var capacityHousing = formNotice.querySelector('#capacity');
 
 // Функции для обработчиков событий
+// Выделение красным цветом рамки поля при ошибочном вводе
+var allocateBorderColor = function (elem) {
+  elem.style.borderWidth = '2px';
+  elem.style.borderColor = 'red';
+};
+// Возвращение рамки в прежнее состояние
+var resetBorderColor = function (elem) {
+  elem.style.borderWidth = '';
+  elem.style.borderColor = '';
+};
+
 // для заголовка
 var onInvalidInput = function () {
+  allocateBorderColor(titleHousing);
   if (titleHousing.validity.tooShort) {
     titleHousing.setCustomValidity('Заголовок должен быть не менее 30-ти символов');
+  } else if (titleHousing.validity.tooLong) {
+    titleHousing.setCustomValidity('Заголовок не должен превышать длинну в 100 символов');
   } else if (titleHousing.validity.valueMissing) {
     titleHousing.setCustomValidity('Обязательное поле');
   } else {
     titleHousing.setCustomValidity('');
+    resetBorderColor(titleHousing);
   }
+};
+var onBlurInput = function (evt) {
+  evt.target.checkValidity();
+};
+
+var onFocusInput = function (evt) {
+  resetBorderColor(evt.target);
 };
 
 // Автоввод времени выезда при изменении времени въезда
@@ -286,66 +308,66 @@ var onChangeTimeIn = function () {
 };
 // Автоввод времени въезда при изменении времени выезда
 var onChangeTimeOut = function () {
-  timeInHousing.selectedIndex = timeOutHousing.selectedIndex;
+  timeInHousing.value = timeOutHousing.value;
 };
 
 // Изменение минимальной стоимости жилья
 var onChangeType = function () {
-  priceHousing.min = offerTypePrice[typeHousing.options[typeHousing.selectedIndex].value];
+  priceHousing.min = offerTypePrice[typeHousing.value];
 };
 
 // Проверка введенной суммы на валидность
 var onInvalidInputPrice = function () {
+  allocateBorderColor(priceHousing);
   if (priceHousing.validity.rangeUnderflow) {
     priceHousing.setCustomValidity('Стоимость жилья ниже рекомендованной');
   } else if (priceHousing.validity.rangeOverflow) {
     priceHousing.setCustomValidity('Стоимость жилья слишком высока');
-  } else if (priceHousing.validity.valueMissing) {
-    priceHousing.setCustomValidity('Обязательное поле');
   } else {
     priceHousing.setCustomValidity('');
+    resetBorderColor(priceHousing);
   }
 };
-
-var capacityOptionActivate = function (i) {
-  capacityHousing.options[i].classList.remove('hidden');
+// Измененная сумма удовлетворяет условиям
+var onChangePrice = function () {
+  resetBorderColor(priceHousing);
+  priceHousing.setCustomValidity('');
 };
 
-var capacityOptionDeActivate = function (i) {
-  capacityHousing.options[i].classList.add('hidden');
+var capacityOptionActivate = function (elem) {
+  elem.classList.remove('hidden');
+};
+
+var capacityOptionDeActivate = function (elem) {
+  elem.classList.add('hidden');
 };
 
 // Изменение select количества гостей в зависимости от изменения количества комнат
 var onChangeRoomNumber = function () {
   var lenCapacitySelectDef = capacityHousing.options.length;
-  var arrCapacitySelect = capacityOfRooms[roomNamberHousing.selectedIndex];
+  var arrCapacitySelect = capacityOfRooms[roomNamberHousing.value];
   var lenCapacitySelect = arrCapacitySelect.length;
+  [].forEach.call(capacityHousing.options, capacityOptionActivate);
   for (var i = 0; i < lenCapacitySelectDef; i++) {
-    capacityOptionActivate(i);
-  }
-  for (i = 0; i < lenCapacitySelectDef; i++) {
     var search = false;
     for (var j = 0; j < lenCapacitySelect; j++) {
-      if (arrCapacitySelect[j] === i) {
+      if (arrCapacitySelect[j] === parseInt(capacityHousing.options[i].value, 10)) {
         search = true;
         break;
       }
     }
     if (!search) {
-      capacityOptionDeActivate(i);
+      capacityOptionDeActivate(capacityHousing.options[i]);
     }
   }
-  capacityHousing.selectedIndex = arrCapacitySelect[0];
-};
-
-// Проверка правильности заполнения формы
-var onSubmitNotice = function () {
-// evt.preventDefault(); Не понимаю, что сюда надо писать, если все поля уже проверяются на этапе ввода данных.
+  capacityHousing.value = arrCapacitySelect[0];
 };
 
 // Обработчики событий
 // проверка ввода заголовка
 titleHousing.addEventListener('invalid', onInvalidInput);
+titleHousing.addEventListener('blur', onBlurInput);
+titleHousing.addEventListener('focus', onFocusInput);
 // Событие изменения времени въезда
 timeInHousing.addEventListener('change', onChangeTimeIn);
 // Событие изменения времени выезда
@@ -354,10 +376,9 @@ timeOutHousing.addEventListener('change', onChangeTimeOut);
 typeHousing.addEventListener('change', onChangeType);
 // Проверка ввода суммы стоимости за ночь
 priceHousing.addEventListener('invalid', onInvalidInputPrice);
+priceHousing.addEventListener('change', onChangePrice);
 // Событие изменения количества комнат
 roomNamberHousing.addEventListener('change', onChangeRoomNumber);
-// Событие отправка формы
-formNotice.addEventListener('submit', onSubmitNotice);
 
 // =========================================================================
 // Инициализация и начало работы
